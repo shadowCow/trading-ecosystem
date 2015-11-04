143e54897220b8a2c337bac8bf9dd5721edcb985
Dan Wadeson <dan.wadeson@tryonsolutions.com>
2015-11-03 21:52:22 -0500

    added some metrics.

--- spark-data-analysis/src/main/scala/com/cowsunday/sparkdataanalysis/transform/PriceChangeRank.scala
+++ spark-data-analysis/src/main/scala/com/cowsunday/sparkdataanalysis/transform/PriceChangeRank.scala
@@ -4,18 +4,18 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.rdd.RDDFunctions._
 import com.cowsunday.sparkdataanalysis.data.PriceBar
 
-object OpenCloseRangeRank {
-
+object PriceChangeRank {
+  
   /**
-   * Ranks the last length bars by the absolute value of (close - open)
+   * Ranks the last length bars by the value (close - open)
    * 
-   * Smallest values (closest to 0) have the lowest rank (lowest rank is 0).
-   * Largest values (farthest from 0) have the highest rank (highest rank is length-1)
+   * Smallest values (most negative) have the lowest rank (lowest rank is 0).
+   * Largest values (most positive) have the highest rank (highest rank is length-1)
    */
   def transform(priceData: RDD[PriceBar], length: Integer): RDD[Integer] = {
     priceData.sliding(length).map(currentSlice => {
       val lastBar = currentSlice.takeRight(1)(0)
-      currentSlice.sortBy(_.getOpenCloseRange).indexOf(lastBar)
+      currentSlice.sortBy(_.getOpenCloseChange).indexOf(lastBar)
     })
   }
 }
---
+++ spark-data-analysis/src/test/scala/com/cowsunday/sparkdataanalysis/transform/LargestPriceChangeTest.scala
@@ -0,0 +1,55 @@
+package com.cowsunday.sparkdataanalysis.transform
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.scalatest.Assertions
+import org.junit.Test
+import org.junit.Before
+import org.junit.After
+
+import com.cowsunday.sparkdataanalysis.data.PriceBar
+
+class LargestPriceChangeTest extends Assertions {
+
+  private val master = "local[2]"
+  private val appName = "LargestPriceChangeTest-test"
+  private val sparkHome = "/usr/local/spark-1.4.1"
+  private val jars = Array[String]("/Users/dwadeson/trading-ecosystem/spark-data-analysis/target/spark-data-analysis-0.0.1-SNAPSHOT.jar")
+  
+  private var sc: SparkContext = _
+  
+  @Before def setup() {
+    val conf = new SparkConf()
+                  .setMaster(master)
+                  .setAppName(appName)
+                  .setSparkHome(sparkHome)
+                  .setJars(jars)
+                  
+    sc = new SparkContext(conf)
+  }
+  
+  @After def after() {
+    if (sc != null) {
+      sc.stop()
+    }
+  }
+  
+  @Test def testTransform() {
+    val priceBars = Seq(new PriceBar(1,2,0.5,3,20150101), 
+        new PriceBar(2,4,1.5,2.5,20150102), 
+        new PriceBar(3,6,2.5,4,20150103),
+        new PriceBar(4,7,3.5,10,20150104),
+        new PriceBar(5,8,4.5,5.5,20150105),
+        new PriceBar(6,9,5.5,13,20150106),
+        new PriceBar(7,10,6.5,9,20150107))
+    val rdd = sc.parallelize(priceBars)
+    
+    val results = LargestPriceChange.transform(rdd, 3).take(5)
+    
+    assert(results(0) == 0)
+    assert(results(1) == 1)
+    assert(results(2) == 0)
+    assert(results(3) == 1)
+    assert(results(4) == 0)
+  }
+}
